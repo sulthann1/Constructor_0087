df3e4422d6ceb15649a9003f2d904519ec364078
sulthann1 <sulthanaawaliya155@gmail.com>
2025-05-13 15:56:58 +0700

    menambahkan int main

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 class mahasiswa{
     public :
-    mahasi
+    mahasiswa(int mhs, string nama);
+};
+
+mahasiswa::mahasiswa(int nim, string nama){
+    cout << "Constructor dengan parameter" <<endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
+
+int main(){
+    mahasiswa mhs(12345, "pascal");
+    return 0;
 }
