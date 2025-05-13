58b0a3ea91a31f5a338cdc727c41e36cba34f88a
sulthann1 <sulthanaawaliya155@gmail.com>
2025-05-13 16:15:35 +0700

    menambahkan  int main

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -31,3 +31,13 @@ void mahasiswa :: cetak(){
     cout << "NIM : " <<nim <<endl;
     cout << "Nama :" <<nama <<endl;
 }
+
+int main(){
+    mahasiswa mhs1(102030);
+    mahasiswa mhs2("Andi");
+    mahasiswa mhs3(010203, "Santi");
+
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+}   
