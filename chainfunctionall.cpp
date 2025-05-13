6ae177f001dfbf357155d1b94fe162e047f55904
sulthann1 <sulthanaawaliya155@gmail.com>
2025-05-13 16:33:14 +0700

    chainfunctionall terakhir

---
+++ chainfunctionall.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class buku{
+    string judul;
+
+    public :
+    buku setJudul(string judul){
+        this -> judul = judul;
+        return*this; 
+    }
+    string getJudul()
+    {
+        return this ->judul;
+    }
+};
+
+int main(){
+    buku bukunya;
+    cout << bukunya.setJudul("Matematika").getJudul();
+    return 0;
+}
